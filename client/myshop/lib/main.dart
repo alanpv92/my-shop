b49a5ad8307dc6a6019a9e40206daeca81739451
Alan Paul Varghese <alan@geekyants.com>
2023-06-09 17:26:19 +0530

    added app theme

--- client/myshop/lib/main.dart
+++ client/myshop/lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:myshop/theme/app.dart';
+import 'package:myshop/ui/screen/authentication.dart';
 
 void main(List<String> args) {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp();
+  Widget build(BuildContext context,WidgetRef ref) {
+    
+    return  MaterialApp(
+      theme: ref.read(appThemeProvider).getLightTheme(),
+      home:const AuthenticationScreen(),
+    );
   }
 }
---
+++ client/myshop/lib/theme/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:myshop/managers/color.dart';
+
+final appThemeProvider = Provider((ref) {
+  final colorManger = ref.read(colorManagerProvider);
+  return AppTheme(colorManager: colorManger);
+});
+
+class AppTheme {
+  final ColorManager colorManager;
+  AppTheme({required this.colorManager});
+  ThemeData getLightTheme() => ThemeData.light().copyWith(
+        primaryColor: colorManager.primaryColor,
+      );
+}
