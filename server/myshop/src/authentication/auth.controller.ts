e80dd2a2d0bc20a9a15cc3f2a8241869782d602c
Alan Paul Varghese <alan@geekyants.com>
2023-06-10 22:11:41 +0530

    refactored app gurd

--- server/myshop/src/authentication/auth.controller.ts
+++ server/myshop/src/authentication/auth.controller.ts
@@ -2,18 +2,22 @@ import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { RegisterUserDto } from "./Dto/register.user";
 import { LoginUserDto } from "./Dto/login.user";
+import { Public } from "src/decorators/public";
+
 
 
 @Controller("/auth")
 export class AuthController{
    constructor(private readonly authService:AuthService){}
-
+ 
     @Post("/register")
+    @Public()
    async registerUser(@Body() data:RegisterUserDto){
         return await this.authService.registerUser(data);
     }
 
     @Post("/login")
+    @Public()
    async loginUser(@Body() data:LoginUserDto){
       return await this.authService.loginUser(data);
     }
--- server/myshop/src/authentication/guards/app.ts
+++ server/myshop/src/authentication/guards/app.ts
@@ -1,14 +1,25 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, HttpException, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class AppGuard implements CanActivate {
-  constructor(private readonly jwt: JwtService) {}
+  constructor(
+    private readonly jwt: JwtService,
+    private readonly reflector: Reflector,
+  ) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublic = this.reflector.getAllAndOverride('isPublic', [
+      context.getClass(),
+      context.getHandler(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
     const http = context.switchToHttp();
     const httpRequest = http.getRequest<Request>();
 
@@ -16,8 +27,20 @@ export class AppGuard implements CanActivate {
       return false;
     }
     const accessToken = httpRequest.headers.authorization.split(' ')[1];
-    const isTokenVaild = this.jwt.verify(accessToken);
+    try {
+      const isTokenVaild = this.jwt.verify(accessToken);
+      if (isTokenVaild) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      if(e.message==='jwt expired'){
+        throw new HttpException("token has expired",400);
+      }
+      return false;
+    }
 
-    return isTokenVaild;
+    return true;
   }
 }
