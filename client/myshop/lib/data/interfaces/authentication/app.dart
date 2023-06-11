df233db7734319a7eaef93ecc5824b007513d554
Alan Paul Varghese <alan@geekyants.com>
2023-06-11 22:23:43 +0530

    app authentication interface

--- client/myshop/lib/data/interfaces/authentication/app.dart
+++ client/myshop/lib/data/interfaces/authentication/app.dart
@@ -1,7 +1,12 @@
 import 'package:myshop/data/custom%20types/types.dart';
+import 'package:myshop/data/models/authentication%20data/login.dart';
+import 'package:myshop/data/models/authentication%20data/registration.dart';
 
-abstract class AppAuthenticationInterface{
-
-
-
+abstract class AppAuthenticationInterface {
+  Future<AppAuthenticationResponse> loginUser(
+      {required LoginAuthenticationDataModel loginAuthenticationDataModel});
+  Future<AppAuthenticationResponse> registerUser(
+      {required RegistrationAuthenticationModel
+          registrationAuthenticationModel});
+  Future logOut();
 }
