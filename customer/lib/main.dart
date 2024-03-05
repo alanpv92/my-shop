c1a4b30cee01d0a5173b07eeac7d58e842378886
Alan Paul Varghese <alan@geekyants.com>
2024-03-05 19:21:11 +0530

    inital commit

---
+++ customer/lib/main.dart
@@ -0,0 +1,16 @@
+import 'common_export.dart';
+
+void main(List<String> args) {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
