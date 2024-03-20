01541d2b90f05efc1a8173e3ccc3a6d724a7cb2f
alan <alan@geekyants.com>
2024-03-20 10:16:42 +0530

    updated code

--- customer/lib/features/onboarding/screens/onboarding.dart
+++ customer/lib/features/onboarding/screens/onboarding.dart
@@ -73,7 +73,7 @@ class _OboardingScreenState extends State<OboardingScreen> {
                   BouncingElevatedButton(
                     text: TStrings.kNext,
                     onPressed: () {
-                      throw Exception();
+                      // throw Exception();
                       changeIndex();
                     },
                   ),
